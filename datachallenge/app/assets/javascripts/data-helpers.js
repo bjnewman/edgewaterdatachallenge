5355748eef6575113c14cd645357692339cbc019
Benjamin Newman <benjaminjnewman@gmail.com>
2016-12-20 19:57:53 -0600

    fix top 10 api call, add placeholder map and begin google map api with basic styling

--- datachallenge/app/assets/javascripts/data-helpers.js
+++ datachallenge/app/assets/javascripts/data-helpers.js
@@ -1,18 +1,19 @@
 // make an API call to Chicago crime data 
 var baseURL = "https://data.cityofchicago.org/resource/6zsd-86xi.json"
-var basicQuery = encodeURI("?$select=ward,COUNT(ward)&$group=ward&#limit=10&order=COUNT(ward) DESC ")
+var basicQuery = encodeURI("?$select=ward,COUNT(ward)&$group=ward&$limit=10&$order=COUNT(ward) DESC ")
 var appToken = "&$$app_token=XTzcaicIeaM3s1GDqbUAaUVS5"
 
 $(document).ready(function(){
 	//test click on dead link to get top ten crime wards
 	$("a").on("click", function(e){
 		e.preventDefault()
+		console.log(baseURL+basicQuery+appToken)
 		$.getJSON( baseURL+basicQuery+appToken, function (data) {
 			console.log(data)
 			var table = "<table> <tr> <th> Number of Crimes </th> <th> Ward </th> </tr>"
 			var tableItems = []
 			// turn each ward with count into table row
-			for (var i = 0; i < data.length; i+=2) {
+			for (var i = 0; i < data.length; i++) {
 				tableItems.push("<tr><td>"+data[i].COUNT_ward+"</td> <td>"+data[i].ward+"</td></tr>")
 			}
 
---
+++ datachallenge/app/assets/javascripts/googMaps.js
@@ -0,0 +1,1 @@
+var baseURL = "https://data.cityofchicago.org/resource/6zsd-86xi.json"
