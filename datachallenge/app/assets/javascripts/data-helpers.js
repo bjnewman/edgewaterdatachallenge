b0712fc89a2b4947222108adb9de08925956b40d
Benjamin Newman <benjaminjnewman@gmail.com>
2016-12-20 20:13:25 -0600

    change grouping by ward to community area for better comparison with chi datasets

--- datachallenge/app/assets/javascripts/data-helpers.js
+++ datachallenge/app/assets/javascripts/data-helpers.js
@@ -1,20 +1,20 @@
 // make an API call to Chicago crime data 
 var baseURL = "https://data.cityofchicago.org/resource/6zsd-86xi.json"
-var basicQuery = encodeURI("?$select=ward,COUNT(ward)&$group=ward&$limit=10&$order=COUNT(ward) DESC ")
+var basicQuery = encodeURI("?$select=community_area,COUNT(community_area)&$group=community_area&$limit=10&$order=COUNT(community_area) DESC ")
 var appToken = "&$$app_token=XTzcaicIeaM3s1GDqbUAaUVS5"
 
 $(document).ready(function(){
-	//test click on dead link to get top ten crime wards
+	//test click on dead link to get top ten crime community_areas
 	$("a").on("click", function(e){
 		e.preventDefault()
 		console.log(baseURL+basicQuery+appToken)
 		$.getJSON( baseURL+basicQuery+appToken, function (data) {
 			console.log(data)
-			var table = "<table> <tr> <th> Number of Crimes </th> <th> Ward </th> </tr>"
+			var table = "<table> <tr> <th> Number of Crimes </th> <th> Community Area </th> </tr>"
 			var tableItems = []
-			// turn each ward with count into table row
+			// turn each community_area with count into table row
 			for (var i = 0; i < data.length; i++) {
-				tableItems.push("<tr><td>"+data[i].COUNT_ward+"</td> <td>"+data[i].ward+"</td></tr>")
+				tableItems.push("<tr><td>"+data[i].COUNT_community_area+"</td> <td>"+data[i].community_area+"</td></tr>")
 			}
 
 			tableItemsString = tableItems.join("")
