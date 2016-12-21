5d8c0c94326fcc15549194aecc65323e80255e10
Benjamin Newman <benjaminjnewman@gmail.com>
2016-12-21 17:31:02 -0600

    add dropdown menu for additional data cuts

--- datachallenge/app/assets/javascripts/data-helpers.js
+++ datachallenge/app/assets/javascripts/data-helpers.js
@@ -24,3 +24,26 @@ $(document).ready(function(){
 		})
 	})
 })
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function showDropdown() {
+    document.getElementById("myDrop").classList.toggle("show");
+    //find option selected
+    
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
