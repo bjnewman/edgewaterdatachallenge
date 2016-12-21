fd4f8db2f047bb895d178d32ea6edc7035e81b4a
Benjamin Newman <benjaminjnewman@gmail.com>
2016-12-20 19:21:30 -0600

    top wards by crime displays on home page after click

--- datachallenge/app/assets/javascripts/data-helpers.js
+++ datachallenge/app/assets/javascripts/data-helpers.js
@@ -1,7 +1,7 @@
 // make an API call to Chicago crime data 
 var baseURL = "https://data.cityofchicago.org/resource/6zsd-86xi.json"
-var basicQuery = encodeURI("?$select=ward,COUNT(ward)&$group=ward&limit=10")
-var appToken = "&$$app_token="+CHIAPPKEY
+var basicQuery = encodeURI("?$select=ward,COUNT(ward)&$group=ward&#limit=10&order=COUNT(ward) DESC ")
+var appToken = "&$$app_token=XTzcaicIeaM3s1GDqbUAaUVS5"
 
 $(document).ready(function(){
 	//test click on dead link to get top ten crime wards
@@ -9,15 +9,16 @@ $(document).ready(function(){
 		e.preventDefault()
 		$.getJSON( baseURL+basicQuery+appToken, function (data) {
 			console.log(data)
-			var table = "<table id = ''> <tr> <th> Ward </th> <th> Number of crimes </th> </tr>"
+			var table = "<table> <tr> <th> Number of Crimes </th> <th> Ward </th> </tr>"
 			var tableItems = []
 			// turn each ward with count into table row
-			$.each(data, function( ward, count) {
-				tableItems.push("<tr><td>"+ward+"</td> <td>"+count+"</td></tr>")
-			})
+			for (var i = 0; i < data.length; i+=2) {
+				tableItems.push("<tr><td>"+data[i].COUNT_ward+"</td> <td>"+data[i].ward+"</td></tr>")
+			}
+
 			tableItemsString = tableItems.join("")
 			table += tableItemsString+"</table>"
-			table.appendTo("#table-container")
+			$("#table-container").html(table)
 		})
 	})
 })
